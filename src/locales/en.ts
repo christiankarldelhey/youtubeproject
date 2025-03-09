40295dd1acecadf0e0ecdd1a11cd7b4fbf19b455
Chris <chris@MacBook-Pro-de-Chris.local>
2025-03-09 05:55:12 -0700

    add dictionary

---
+++ src/locales/en.ts
@@ -0,0 +1,10 @@
+export default {
+    welcome: "Hola Mundo",
+    login: "Login",
+    logout: "Logout",
+    profile: {
+      title: "Your Profile",
+      edit: "Edit Profile",
+      save: "Save Changes",
+    },
+  };
---
+++ src/locales/index.ts
@@ -0,0 +1,17 @@
+import { createI18n } from 'vue-i18n';
+import en from './en.ts';
+// import es from './es';
+
+const messages = {
+  en,
+//   es,
+};
+
+const i18n = createI18n({
+  legacy: false,
+  locale: 'en',
+  fallbackLocale: 'en',
+  messages,
+});
+
+export default i18n;
