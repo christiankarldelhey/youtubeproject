743b6a7ba101f493a3dbdc3e01672f8770d0fe09
Chris <chris@MacBook-Pro-de-Chris.local>
2025-03-09 06:31:31 -0700

    import tailwind and design system

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,18 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import { useI18n } from 'vue-i18n';
+import Button from 'primevue/button';
 
 const { t } = useI18n();
 
 defineProps<{ msg: string }>()
 
-const count = ref(0)
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <Button label="button" />
     <p>
       {{ t('welcome') }}
     </p>
